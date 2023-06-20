dc31035a41419f4a65f6cfa060e72bb17dbed496
gholizadeh.m <gholizadeh.m@ronixtools.com>
2023-06-20 17:26:57 +0330

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+
+
+<template>
+  <div>
+   <HeaderWebsite></HeaderWebsite>
+  </div>
+</template>
+<script>
+import HeaderWebsite from './components/Header.vue';
+
+export default {
+  components: {
+    HeaderWebsite
+  },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="container px-0">
+  <div class="row mx-0 px-0 py-3">
+      <div class="col-2">
+        <img class="img-fluid m-auto" src="../assets/img/logo.png" />
+      </div>
+      <div class="col-7 ">
+        <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">About Us</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">Contact US</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link disabled" href="#">
+              Shop
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="col-3 d-flex justify-content-between">
+  <input type="text" class="form-control" placeholder="serch" >
+
+  <button type="button" class="btn btn-primary ms-2 px-3 py-">Login</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
