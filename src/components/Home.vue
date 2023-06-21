6a69b2f6377c3687e80485dac2f014f77e6e6a23
gholizadeh.m <gholizadeh.m@ronixtools.com>
2023-06-21 16:45:22 +0330

    add loading

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,11 +1,15 @@
 <template>
+    <pulse-loader :loading="loading" :color="blue" :size="size"></pulse-loader>
     home page
     <button @click="goTocontact" class="bg-primary border-0 text-white px-2 py-2">Go to Contact</button>
 </template>
 
 <script>
-import { useRouter} from 'vue-router'
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 export default {
+    components:{
+        PulseLoader
+    },
     methods:{
      goTocontact(){
         console.log('hi')
--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap";
 import router from './router'
+import PulseLoader from 'vue-spinner/src/PulseLoader.vue'
 
 
 let app = createApp(App);
 
 app.use(router);
+app.use(PulseLoader);
 app.mount('#app')
