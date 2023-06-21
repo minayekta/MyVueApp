675b786106aa9c2768f22220565e9f8557e9137b
gholizadeh.m <gholizadeh.m@ronixtools.com>
2023-06-21 14:36:56 +0330

    GET DATA from api for blogs

--- src/components/Blog.vue
+++ src/components/Blog.vue
@@ -1,46 +1,57 @@
 <template>
   <div class="container px-0">
-    <div class="row mx-0">
-      <h1>
-        Blogs
-      </h1>
-      <div class="col-4 card">
-        <img class="img-fluid" src="" />
-        <p>
-        
-        </p>
+    <div v-if="!loading">
+      <div class="row mx-0">
+        <h1>
+          Blogs
+        </h1>
+        <div v-for="post in posts" class="col-5 card mb-3 me-3">
+          <img class="img-fluid" src="" />
+          <h4>
+          {{ post.title }}
+          </h4>
+          <p>
+           {{ post.body }}
+          </p>
+        </div>
       </div>
     </div>
+    <div v-else="">Loading ...</div>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
-
+import _ from 'underscore'
 export default {
-    data(){
-        return{
-            "userId": 1,
-            "id": 1,
-            "title": "test",
-            "body" : ""
-        }
-    },
-  mounted() {
+  data() {
+    return {
+      posts: null,
+      loading: false,
+    }
+  },
+  created() {
+    this.loading = true;
     this.getAllposts()
   },
   methods: {
- 
     getAllposts() {
-        this.posts = []
+      this.posts = []
       axios
-        .get('https://jsonplaceholder.typicode.com/posts')
+        .get('https://jsonplaceholder.typicode.com/posts?_page=10_limit=9')
         .then((res) => {
-            this.posts = res.data;
-            console.log('posts',this.posts.$slots)
+            this.loading = false
+          this.posts = res.data
+          console.log('posts', this.posts)
+          console.log('posts', _.chunk(res.data,2))
         })
-       
+        .catch((err) => alert(err))
     },
   },
 }
 </script>
+<style scoped>
+.card{
+    padding: 8px;
+}
+</style>
